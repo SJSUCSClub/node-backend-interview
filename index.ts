0ffc7e21b14f880169d70bb3c34369cff271e1ca
ManasC478 <manas.chougule@gmail.com>
2025-02-25 00:28:52 -0800

    init

---
+++ index.ts
@@ -0,0 +1,42 @@
+import express from 'express';
+
+const app = express();
+app.use(express.json());
+
+type Review = {
+  id: number;
+  professorName: string;
+  courseName: string;
+  rating: number;
+  grade: string;
+  tags: string[];
+  takeAgain: boolean;
+  content: string;
+}
+
+const reviews: Review[] = [
+  {
+    id: 1,
+    professorName: 'John Doe',
+    courseName: 'Introduction to Computer Science',
+    rating: 5,
+    grade: 'A',
+    tags: ['Java', 'Programming'],
+    takeAgain: true,
+    content: 'This is a great course!'
+  },
+  {
+    id: 2,
+    professorName: 'Jane Smith',
+    courseName: 'Advanced Computer Science',
+    rating: 4,
+    grade: 'B',
+    tags: ['Java', 'DSA'],
+    takeAgain: false,
+    content: 'This course is challenging but worth it.'
+  }
+];
+
+app.listen(3000, () => {
+  console.log('App listening on port 3000!');
+});
